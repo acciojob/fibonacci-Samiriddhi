92a43a2d13c9521a4579dc8b1270375ddc9a9f87
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-07-20 00:16:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 function fibonacci(num) {
 // your code here
-	if(num=0){
-		return 0;
-	}else if(num=1){
-		return 1;
-	}
-	else{
-		return (num-1)+(num-2);
-	 }
+	  if (num <= 1) {
+    return num;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 }
 
 module.exports = fibonacci;
